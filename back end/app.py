bfedb1521c2fb697f38cbc8067fe86c06ce6af2c
akhilesh <sit24ad048@sairamtap.edu.in>
2025-08-06 13:22:59 +0530

    final changes before deploy

--- back end/app.py
+++ back end/app.py
@@ -434,3 +434,7 @@ if __name__ == '__main__':
     print("🔧 API endpoint available at: http://localhost:5000/api/recommend")
     app.run(debug=True, host='0.0.0.0', port=5000)
     
+    import os
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
